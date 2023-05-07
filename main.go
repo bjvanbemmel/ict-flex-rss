9edf1ea0fee72944961e349b997a5041a39c7f18
bjvanbemmel <97085143@st.deltion.nl>
2023-05-07 19:19:27 +0200

    Moved the structs into the newly created types package

--- main.go
+++ main.go
@@ -7,43 +7,10 @@ import (
 	"log"
 	"time"
 
+    . "github.com/bjvanbemmel/ict-flex-rss/types"
 	"github.com/gocolly/colly/v2"
 	"github.com/gosimple/slug"
 )
-
-type Article struct {
-	Guid        ArticleGuid `xml:"guid"`
-	Link        string      `xml:"link"`
-	Title       string      `xml:"title"`
-	Description string      `xml:"description"`
-	Author      Author      `xml:"author"`
-	CreatedAt   time.Time   `xml:"pubDate"`
-}
-
-type ArticleGuid struct {
-	Id          string `xml:",chardata"`
-	IsPermaLink bool   `xml:"isPermaLink,attr"`
-}
-
-type Author struct {
-	Name    string `xml:"name"`
-	Profile string `xml:"profile"`
-}
-
-type Feed struct {
-	XMLName     xml.Name   `xml:"channel"`
-	Title       string     `xml:"title"`
-	Description string     `xml:"description"`
-	Link        string     `xml:"link"`
-	Articles    []*Article `xml:"item"`
-}
-
-var ArticleFeed Feed = Feed{
-	Articles:    []*Article{},
-	Title:       "ICT-Flex - Announcements",
-	Link:        "https://ict-flex.nl/category/mededelingen/",
-	Description: "RSS Feed for ICT-Flex announcements",
-}
 
 func main() {
 	c := colly.NewCollector()
