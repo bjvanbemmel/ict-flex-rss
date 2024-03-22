822cb40d6ee9c98cd95144e3068c9a9cba952706
bjvanbemmel <97085143@st.deltion.nl>
2024-03-22 20:45:25 +0100

    removed comment

--- main.go
+++ main.go
@@ -88,55 +88,6 @@ func main() {
 		})
 	})
 
-	// c.OnHTML("html", func(e *colly.HTMLElement) {
-	// 	nodes := e.DOM.Children().Find("body>main#content.type-post").Nodes
-
-	// 	if len(nodes) < 1 {
-	// 		return
-	// 	}
-
-	// 	var article Article
-
-	// 	e.ForEach("head>meta[property]", func(_ int, e *colly.HTMLElement) {
-	// 		content := e.Attr("content")
-
-	// 		switch e.Attr("property") {
-	// 		case "og:url":
-	// 			article.Link = content
-	// 		case "og:title":
-	// 			article.Title = content
-	// 		case "og:description":
-	// 			article.Description = content
-	// 		case "article:published_time":
-	// 			date, err := time.Parse(DATE_FORMAT, content)
-	// 			if err != nil {
-	// 				log.Fatal(err)
-	// 				return
-	// 			}
-	// 			article.CreatedAt = date
-	// 		}
-	// 	})
-
-	// 	var id string
-	// 	post := post_regex.FindString(e.ChildAttr("main#content", "class"))
-	// 	if post == "" {
-	// 		log.Println("Post ID could not be found")
-	// 	} else {
-	// 		id = strings.Split(post, "-")[1]
-	// 	}
-
-	// 	article.Guid.Id = id
-	// 	article.Author = e.ChildAttr("head>meta[name='twitter:data1']", "content")
-
-	// 	ArticleFeed.Articles = append(ArticleFeed.Articles, &article)
-	// })
-
-	// c.OnHTML("div.elementor-posts", func(e *colly.HTMLElement) {
-	// 	e.ForEach("article", func(_ int, e *colly.HTMLElement) {
-	// 		e.Request.Visit(e.ChildAttr(".elementor-post__card>.elementor-post__text>.elementor-post__title>a", "href"))
-	// 	})
-	// })
-
 	if err := c.Visit("https://ict-flex.nl/mededelingen/"); err != nil {
 		log.Fatal(err)
 		return
